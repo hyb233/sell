362a6390b75ebc20dbec16bb976e8e3a02bf3f7d
hyb <beau_233@163.com>
2019-02-16 15:03:44 +0800

    订单模块完结

---
+++ src/main/java/com/sell/utils/JsonUtil.java
@@ -0,0 +1,12 @@
+package com.sell.utils;
+
+/******
+ *author:hyb
+ *date:2019/2/14  13:17
+ *description:
+ */
+public class JsonUtil {
+    /*public static String toJson(Object object){
+
+    }*/
+}
---
+++ src/main/java/com/sell/utils/MathUtil.java
@@ -0,0 +1,13 @@
+package com.sell.utils;
+
+/******
+ *author:hyb
+ *date:2019/2/14  16:05
+ *description:
+ */
+public class MathUtil {
+
+    /*public static Boolean equals(){
+
+    }*/
+}
